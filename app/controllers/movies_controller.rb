1782c887e96c18cae63a6c9e56e7f60d78a664e1
tinggao <tingting-gao@uiowa.edu>
2014-07-05 13:02:14 -0500

    initial commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,49 @@
+class MoviesController < ApplicationController
+	def index
+		@movies = Movie.all.sort_by &:title
+	end
+
+	def edit		
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+	  @movie = Movie.find params[:id]
+	  @movie.update_attributes!(movie_params)
+	  flash[:notice] = "#{@movie.title} was successfully updated."
+	  redirect_to movie_path(@movie)
+	end
+
+	def show
+		id = params[:id]
+		@movie = Movie.find(id)
+	rescue ActiveRecord::RecordNotFound
+		flash[:notice] = "movie with id #{id} does not exist"
+		redirect_to movies_path
+	end
+
+	def new
+	end
+
+	def create		
+		@movie = Movie.new(movie_params)
+		@movie.save!
+		flash[:notice] = "#{@movie.title} was sucessfully created"
+		redirect_to movie_path(@movie)
+	end
+
+	def destroy
+	  @movie = Movie.find(params[:id])
+	  @movie.destroy
+	  flash[:notice] = "Movie #{@movie.title} was sucessfully deleted."
+	  redirect_to movies_path
+	end
+
+	private
+
+	def movie_params
+		params.require(:movie).permit(:title, :rating, :description, :release_date)
+	end
+
+	
+end
